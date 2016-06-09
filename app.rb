74f28ca446f549222c3cb3747cfbf6838b63e2e8
Tim Moreton <tmoreton89@gmail.com>
2016-06-08 22:25:46 -0400

    working charging on test suite for stripe

--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
 require 'sinatra'
+require 'stripe'
+
+Stripe.api_key = settings.secret_key
 
 get '/' do
-  "Hello World!"
+  erb :index
+end
+
+post '/charge' do
+  @amount = 500
+
+  customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :card  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Sinatra Charge',
+    :currency    => 'usd',
+    :customer    => customer
+  )
+
+  erb :charge
 end
