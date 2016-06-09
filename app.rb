1d6e788d29d7bc6b37d2a20567c1bfa03103b422
Tim Moreton <tmoreton89@gmail.com>
2016-06-08 22:31:39 -0400

    working charging on test suite for stripe

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 require 'stripe'
 
-Stripe.api_key = settings.secret_key
+set :publishable_key, ENV['pk_test_OxISGD7GxGhZCOofus3QFoW8']
+set :secret_key, ENV['sk_test_Cv1UxGFrtA7dBCrUWstCn5sA']
+
+# Stripe.api_key = settings.secret_key
+Stripe.api_key = 'sk_test_Cv1UxGFrtA7dBCrUWstCn5sA'
 
 get '/' do
   erb :index
